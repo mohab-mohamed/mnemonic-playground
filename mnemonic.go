8f4379e87f69266d7cb3849eec5bc2d9a056ef22
Mohab Mohamed <mohab8m@gmail.com>
2022-04-18 19:32:55 -0400

    add helper functions for checksum

--- mnemonic.go
+++ mnemonic.go
@@ -1,11 +1,91 @@
 package main
 
 import (
-	"strconv"
+	// "strconv"
 	"strings"
 	"crypto/rand"
+	"crypto/sha256"
 	"fmt"
+	"os"
+	"bufio"
 )
+
+func ByteArrayToBitString(b []byte) string {
+	var sb strings.Builder
+	for _, n := range(b) {
+		binary := fmt.Sprintf("%08b", n) // prints 00000000 11111101
+		sb.WriteString(binary)
+    	}
+	return sb.String()
+}
+
+func AddChecksum(entropy string, sha256 string) string {
+	var sb strings.Builder
+	sb.WriteString(entropy)
+	for i := 0; i < len(entropy)/32; i++ {
+		fmt.Println(string(entropy[i]))
+		sb.WriteString(string(entropy[i]))
+    	}
+	fmt.Println("checksum: ", sb.String())
+	return sb.String()
+}
+
+func Chunks(s string, chunkSize int) []string {
+	if len(s) == 0 {
+	    return nil
+	}
+	if chunkSize >= len(s) {
+	    return []string{s}
+	}
+	var chunks []string = make([]string, 0, (len(s)-1)/chunkSize+1)
+	currentLen := 0
+	currentStart := 0
+	for i := range s {
+	    if currentLen == chunkSize {
+		chunks = append(chunks, s[currentStart:i])
+		currentLen = 0
+		currentStart = i
+	    }
+	    currentLen++
+	}
+	chunks = append(chunks, s[currentStart:])
+	return chunks
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+	    return nil, err
+	}
+	defer file.Close()
+    
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+	    lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func BitStringToNumber(bitString string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+	    return nil, err
+	}
+	defer file.Close()
+    
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+	    lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
 
 
 func main() {
@@ -16,26 +96,27 @@ func main() {
 		fmt.Println("error:", err)
 		return
 	}
-	var sb strings.Builder
-	for i, n := range(b) {
-		fmt.Println(i)
-		binary := fmt.Sprintf("%b", n) // prints 00000000 11111101
-		fmt.Println(binary)
-		fmt.Printf("%d\n\n", n)
-		sb.WriteString(binary)
-    	}
-	entropy := sb.String()
+	fmt.Println(b)
+	entropy := ByteArrayToBitString(b)
 	fmt.Println()
-	fmt.Println(entropy)
+	fmt.Println("entropy: ", entropy, len(entropy))
 
-	output, err := strconv.ParseInt(entropy[:11], 2, 64)  
-	if err != nil {  
-	fmt.Println(err)  
-	return  
-	}  
+	// output, err := strconv.ParseInt(entropy[:11], 2, 64)  
+	// if err != nil {  
+	// fmt.Println(err)  
+	// return  
+	// }  
 	
-	fmt.Println()
-	fmt.Println(entropy[:11])
-	fmt.Printf("Output %d\n", output)  
+	// fmt.Println()
+	// fmt.Println(entropy[:11])
+	// fmt.Printf("Output %d\n", output)  
 
+	hash := sha256.Sum256(b)
+	hashString := ByteArrayToBitString(hash[:])
+	fmt.Println("sha256 hash: ", hashString, len(hashString))
+	withChecksum := AddChecksum(entropy, hashString)
+	fmt.Println("entropy + checksum: ", withChecksum, len(withChecksum))
+	fmt.Println("chunks: ", Chunks(withChecksum, 11))
+	wordArray, err := ReadLines("wordlist.txt")
+	fmt.Println("word array: ", len(wordArray))
 }
